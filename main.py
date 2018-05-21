a437febe62983717d93c2922126977affbe42ad7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-05-21 16:37:00 +0700

    save Word tokenizer

--- main.py
+++ main.py
@@ -5,18 +5,29 @@ V 0.1
 พัฒนาโดย นาย วรรณพงษ์  ภัททิยไพบูลย์
 """
 import logging
-from flask import Flask
+from flask import Flask, render_template, abort, request,jsonify
 from pythainlp.tokenize import word_tokenize
 from pythainlp.tokenize import tcc
-from pythainlp.word_vector import thai2vec
+#from pythainlp.word_vector import thai2vec
 from pythainlp.tag import pos_tag
 from pythainlp.sentiment import sentiment
 from pythainlp.soundex import LK82,Udom83
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hi"
+
+@app.route('/word_tokenizer')
+def word_tokenizer_web():
+    return render_template('word_tokenizer.html', name='Word tokenizer')
+
+@app.route('/api/word_tokenizer', methods=["GET"])
+def word_tokenizer_api():
+	sent = request.args.get('sent', 0, type=str)
+	txt='|'.join(word_tokenize(sent)).replace('|<|br|>|','<br>')
+	return jsonify(result=txt)
 
 
 @app.errorhandler(500)
